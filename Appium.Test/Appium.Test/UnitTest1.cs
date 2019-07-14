b05d2318b61e951c79e43bf24e2a35ab03a2c74c
Mahmoud Abuzaid <mahmoud.a.zaid@outlook.com>
2019-07-14 11:58:16 +0300

    Set Capabilities

--- Appium.Test/Appium.Test/UnitTest1.cs
+++ Appium.Test/Appium.Test/UnitTest1.cs
@@ -1,14 +1,28 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using System;
 
 namespace Appium.Test
 {
     [TestClass]
     public class UnitTest1
     {
+        // Creating instance of Appium Driver
+        AppiumDriver<IWebElement> driver;
+
         [TestMethod]
         public void TestMethod1()
         {
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.SetCapability("platformName", "Android");
+            cap.SetCapability("platformVersion", "7.1.1");
+            cap.SetCapability("deviceName", "Nexus6p");
+            cap.SetCapability("app", "D:\\Repository\\Appium.Test\\APKs\\APKPure.apk");          
+
+            driver = new AndroidDriver<IWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"),cap);
         }
     }
 }
